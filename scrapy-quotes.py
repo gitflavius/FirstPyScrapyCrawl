d0754b6b0c50ef2f14554b94acaa08fbec72047a
Flavio Augusto Rodrigues <flavius.rodrigues@gmail.com>
2024-03-28 21:32:19 -0300

    PyWebScrapy
    PyWebScrapy

---
+++ scrapy-quotes.py
@@ -0,0 +1,20 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = 'QuotesSpider'
+    start_urls = ['https://quotes.toscrape.com/']
+
+    def parse(self, response):
+        quotes = response.xpath('*//div[@class="quote"]')
+        for q in quotes:
+            yield {
+            'title':q.xpath('.//span[@class="text"]/text()').get(),
+            'author':q.xpath('.//small[@class="author"]/text()').get(),
+            'tags':q.xpath('.//div[@class="tags"]/a[@class="tag"]/text()').getall()
+            }
+        
+        next_pag = response.xpath('*//li[@class="next"]/a/@href').get()
+        
+        if next_pag is not None:
+            yield scrapy.Request(response.urljoin(next_pag), callback=self.parse)
+        
